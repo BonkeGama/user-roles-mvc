f2a651f1e126456b3b17f3cf16300956a33b3f4a
Vince <78021809+BonkeGama@users.noreply.github.com>
2021-01-27 14:28:19 +0200

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Rolezz.Startup))]
+namespace Rolezz
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
