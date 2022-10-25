645dd3c3fa940353e4a0f3840911d1594370887b
Bonke Gama <Bonkeg18@gmail.com>
2022-10-25 13:01:33 +0200

    Update UsersView.cs

--- Models/UsersView.cs
+++ Models/UsersView.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace GqihhaLamlaji.Model.ViewModels
+namespace Roles.Model.ViewModels
 {
     public class UsersView
     {
