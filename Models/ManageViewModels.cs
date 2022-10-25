8a31b3b76cef5f589f5e02739bd09ee3f6f6644d
Bonke Gama <Bonkeg18@gmail.com>
2022-10-25 13:02:52 +0200

    Update ManageViewModels.cs

--- Models/ManageViewModels.cs
+++ Models/ManageViewModels.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNet.Identity;
 using Microsoft.Owin.Security;
 
-namespace Rolezz.Models
+namespace Roles.Models
 {
     public class IndexViewModel
     {
